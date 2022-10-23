930908cd07cdebb13d53b1c0cbe16815309e8d50
Ewa Paszkowska <ewqa89@gmail.com>
2022-10-23 17:56:19 +0200

    changes funktion js

--- js/script.js
+++ js/script.js
@@ -3,17 +3,22 @@
         console.log("Cześć!");
     }
 
-    welcome();
-    
-let mainButton = document.querySelector(".js-mainButton");
-let mainImgeActivity = document.querySelector(".js-mainImageActivity");
-let highFiveButton = document.querySelector(".js-HighFiveButton");
+    const toggleImageActivityClick = () => {
+        const mainImgeActivity = document.querySelector(".js-mainImageActivity");
+        const highFiveButton = document.querySelector(".js-HighFiveButton");
 
-mainButton.addEventListener("click", () => {
-    mainImgeActivity.classList.toggle("main__ImgeActivity");
-    mainImgeActivity.classList.toggle("main__ImgeNonActivity");
+        mainImgeActivity.classList.toggle("main__ImgeActivity");
+        mainImgeActivity.classList.toggle("main__ImgeNonActivity");
 
-    highFiveButton.innerText = mainImgeActivity.classList.contains("main__ImgeActivity") ? "Przybij piątkę" : "Nie przybijaj piątki" ;
+        highFiveButton.innerText = mainImgeActivity.classList.contains("main__ImgeActivity") ? "Przybij piątkę" : "Nie przybijaj piątki";
+    };
 
-});
+    const init = () => {
+        const mainButton = document.querySelector(".js-mainButton");
+        mainButton.addEventListener("click", toggleImageActivityClick);
+        
+        welcome();
+
+    };
+    init();
 }
