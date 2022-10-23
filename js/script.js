ef7d9368f3aa32fa7a4c2e9df6563f3364b88196
Ewa Paszkowska <ewqa89@gmail.com>
2022-10-23 18:06:19 +0200

    changed imge to image

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@
         const mainImageActivity = document.querySelector(".js-mainImageActivity");
         const highFiveButton = document.querySelector(".js-HighFiveButton");
 
-        mainImageActivity.classList.toggle("main__ImgeActivity");
-        mainImageActivity.classList.toggle("main__ImgeNonActivity");
+        mainImageActivity.classList.toggle("main__ImageActivity");
+        mainImageActivity.classList.toggle("main__ImageNonActivity");
 
-        highFiveButton.innerText = mainImageActivity.classList.contains("main__ImgeActivity") ? "Przybij piątkę" : "Nie przybijaj piątki";
+        highFiveButton.innerText = mainImageActivity.classList.contains("main__ImageActivity") ? "Przybij piątkę" : "Nie przybijaj piątki";
     };
 
     const init = () => {
